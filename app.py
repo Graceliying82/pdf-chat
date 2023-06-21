85dc061b403e8dd7c56ca7ecd8babbe6f1d8eac2
Hiro <laciferin@gmail.com>
2023-06-21 19:14:18 +0530

    fix: pdf_reader
    Signed-off-by: Hiro <laciferin@gmail.com>

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-    for page in pdf_reader.pages:
-        text += page.extract_text()
+        for page in pdf_reader.pages:
+            text += page.extract_text()
     return text
 
 
