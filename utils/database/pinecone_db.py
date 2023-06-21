3610d215e2bc6f5c6696a7a6240360c0710d3637
Hiro <laciferin@gmail.com>
2023-06-21 18:32:23 +0530

    fix: index_list
    Signed-off-by: Hiro <laciferin@gmail.com>

--- utils/database/pinecone_db.py
+++ utils/database/pinecone_db.py
@@ -2,31 +2,34 @@ import pinecone
 
 from constants import PINECONE_API_KEY, PINECONE_API_ENV
 
+from functools import cache
+
+
+def setup_pinecone():
+    pinecone.init(
+        api_key=PINECONE_API_KEY,
+        environment=PINECONE_API_ENV,
+    )
+
 
 class PineconeDB:
-    # Index list get from database
-    index_list = []
 
     def __init__(self):
-        # get required API keys to init database
         pinecone.init(
             api_key=PINECONE_API_KEY,
             environment=PINECONE_API_ENV,
         )
-        print('inside class PineconeDB. initializing...')
-        #self.get_indexes()
 
-    # Function to get existed API keys.
-    def get_indexes(self):
-        self.index_list = pinecone.list_indexes()
-        return self.index_list
+    @property
+    @cache  # FIX: can cause problems
+    def index_list(self):
+        return pinecone.list_indexes()
 
     # Function to creat a new Index, default dimension is 1536 and default metric is "euclidean"
     def create_index(self, index_name, dimension=1536, metric="euclidean"):
         # get the latest indexes list from server
-        self.get_indexes()
-       
-       # check before creating
+
+        # check before creating
         if index_name not in self.index_list:
             # index not existed. Create a new index
             pinecone.create_index(name=index_name, dimension=dimension, metric=metric)
