a3ea8e05dd3072553cf9b29451c95f22074ac5eb
Hiro <laciferin@gmail.com>
2023-06-21 19:19:44 +0530

    dev: types
    Signed-off-by: Hiro <laciferin@gmail.com>

--- utils/inputs/pdf.py
+++ utils/inputs/pdf.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 from PyPDF2 import PdfReader
+from typing.io import IO
 
 
-def parse_pdfs(pdf_docs):
+def parse_pdfs(pdf_docs: str | IO | Path):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
